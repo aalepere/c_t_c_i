4f26a8e3cf9c23cddc4f850a8e784f783a1adcae
Arnaud <arnaud@hokodo.co>
2019-12-08 18:50:28 +0100

    balances parenthesis

--- scripts/chapter1_array_and_strings.py
+++ scripts/chapter1_array_and_strings.py
@@ -23,3 +23,18 @@ def check_all_charac_unique_no_set(string):
         else:
             test_list.append(s)
     return True
+
+
+def check_if_all_parathensis_balanced(string):
+    """ Check that all the parenthesis in the chain are balanced, opening and closing """
+    test_list = []
+
+    for s in string:
+        if s == "(":
+            test_list.append(s)
+        if s == ")":
+            x = test_list.pop()
+    if len(test_list):
+        return False
+    else:
+        return True
